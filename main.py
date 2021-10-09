f66fa1f127fa76d81ba8902a93ba935db840e20e
KoreanKillersNo1 <64729291+KoreanKillersNo1@users.noreply.github.com>
2021-10-09 18:52:35 +0900

    Create main.py

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ async def 도움(ctx):
 
 @bot.command()
 async def 개발자(ctx):
-    embed = discord.Embed(title = "개발자", description = "『킬쟁이』#1798", color = 0x6E1753
+    embed = discord.Embed(title = "개발자", description = "『킬쟁이』#1798", color = 0x6E1753)
+    await ctx.send(embed=embed)
     
 @bot.command()
 async def 주사위(ctx):
