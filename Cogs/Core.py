f7408fe65919c94037348bdca2ee7a01698bcf02
KoreanKillersNo1 <64729291+KoreanKillersNo1@users.noreply.github.com>
2021-10-10 21:49:27 +0900

    Update Core.py

--- Cogs/Core.py
+++ Cogs/Core.py
@@ -10,6 +10,26 @@ class Core(commands.Cog):
     async def printit(self, ctx): #수정
         await ctx.send(":) Python Bot에 의해 출력됨.")
     
+    @command.command(name="참여")
+    async def join(self, ctx):
+        try:
+            global vc
+            vc = await ctx.message.author.voice.channel.connect()
+            
+        except:
+            try:
+                await ctx.move_to(ctx.message.author.voice.channel)
+            except:
+                await ctx.send("채널에 유저가 접속해있지 않네요..")
+    
+    @command.command(name="나가")
+    async def leave(self, ctx):
+        try:
+            await vc.disconnect()
+        except:
+                await ctx.send("이미 그 채널에 속해있지 않아요!")
+        
+    
     
 
 def setup(bot):
